5a463e8ece7f3ef40a548c68a253c11e39c2521b
Josep Egea <jes@josepegea.com>
2021-07-27 12:09:48 +0200

    Improved Scanner specs

--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+# Clipper subclass that keeps track of scanned ticks
 class TracedClipper < Aliens::Clipper
   attr_reader :traced_tick_map
   attr_accessor :tick_hits
@@ -55,6 +56,31 @@ describe Aliens::Scanner do
     it "scans all the ticks in the reading" do
       results = scanner.scan(reading)
       expect(traced_clipper.tick_hits.flatten.any? { |v| v == 0 }).to be false
+    end
+  end
+
+  describe "collecting positives" do
+    let(:dumb_matcher) { Aliens::Matcher.new }
+    let(:scanner) { Aliens::Scanner.new([alien], matcher: dumb_matcher) }
+    let(:responses) { [0, 0, 0.5, 0.9, 0.8, 0.2, 0.7, 0.85, 0] }
+
+    before do
+      allow(dumb_matcher).to receive(:match).and_return(*responses)
+    end
+
+    it "returns the matching aliens with default confidence factor" do
+      results = scanner.scan(reading)
+      expect(results.size).to eq(2)
+      expect(results.map(&:confidence_factor)).to eq([0.9, 0.85])
+    end
+
+    it "returns the matching aliens with custom confidence factor" do
+      scanner = Aliens::Scanner.new([alien],
+                                    matcher: dumb_matcher,
+                                    minimum_confidence_factor: 0.7)
+      results = scanner.scan(reading)
+      expect(results.size).to eq(4)
+      expect(results.map(&:confidence_factor)).to eq([0.9, 0.8, 0.7, 0.85])
     end
   end
 end
