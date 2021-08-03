071fda7037d4fce73e17ac8761814ed970840796
Josep Egea <jes@josepegea.com>
2021-08-03 10:50:19 +0200

    Added CLI options for choosing patterns and edge thereshold

--- lib/aliens/cli.rb
+++ lib/aliens/cli.rb
@@ -7,11 +7,14 @@ module Aliens
   class CLI < Thor
     desc "scan", "Scan a radar reading looking for Aliens"
     option :minimum_confidence_factor, type: :numeric, aliases: 'c', default: 0.85
+    option :edge_thereshold, type: :numeric, aliases: 'e', default: 0.5
+    option :patterns, type: :array, aliases: 'p', default: %w[data/alien01.txt data/alien02.txt]
 
     def scan(reading = 'data/radar01.txt')
       alien_patterns = load_aliens
       aliens_scanner = Aliens::Scanner.new(alien_patterns,
-                                           minimum_confidence_factor: options[:minimum_confidence_factor])
+                                           minimum_confidence_factor: options[:minimum_confidence_factor],
+                                           edge_thereshold: options[:edge_thereshold])
       radar_reading = parser.parse(File.read(reading))
 
       bandits = aliens_scanner.scan(radar_reading)
@@ -28,10 +31,9 @@ module Aliens
     end
 
     def load_aliens
-      alien_patterns = []
-      alien_patterns << parser.parse(File.read('data/alien01.txt'))
-      alien_patterns << parser.parse(File.read('data/alien02.txt'))
-      alien_patterns
+      options[:patterns].map do |pattern|
+        parser.parse(File.read(pattern))
+      end
     end
 
     def show_results(bandits)
